74c8d4afeab6f8effe2c6d8c81a1a8dcdd51e642
TheFurriestBoy <108354920+TheFurriestBoy@users.noreply.github.com>
2022-06-28 10:36:52 -0500

    Add files via upload

---
+++ Data_Stealer.py
@@ -0,0 +1,25 @@
+# name stealer
+
+name = input("What is your name?: ")
+
+# age stealer
+
+age = input("How old are you?: ")
+
+# location stealer
+
+city = input("What city do you live in?: ")
+
+# hobby stealer
+
+hobby = input("What do you enjoy doing?: ")
+
+# compile stolen data
+
+compiledData = "Your name is {}. You are {} years old, and live in {}. You enjoy {}.".format(name, age, city, hobby)
+
+# the data we stole
+
+print(compiledData)
+
+
